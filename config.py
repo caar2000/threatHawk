84409c185a34098205f2937a52b7db428707dc2b
hawkbluedevil <hawkbluedevil@users.noreply.github.com>
2015-12-09 23:46:36 -0500

    Create config.py
    initial version

---
+++ config.py
@@ -0,0 +1,9 @@
+#--------------------------------------------------------------------------------
+# These are API keys used throughout the script
+#--------------------------------------------------------------------------------
+
+consumer_key = ""
+consumer_secret = ""
+access_key = ""
+access_secret = ""
+shodan_api_key = ""
